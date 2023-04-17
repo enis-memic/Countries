2a40e336a9317c9e8c167929ae7047ed739066ae
Enis <enis.mema@gmail.com>
2023-04-17 13:14:57 +0200

    Fix mobile styling bugs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { FaMicrophone, FaEllipsisH } from 'react-icons/fa';
-import { SlArrowLeft } from 'react-icons/sl';
+import { FaMicrophone, FaEllipsisH, FaChevronLeft } from 'react-icons/fa';
 import './Navbar.css';
 
 function Navbar() {
   return (
     <div className="navigation">
       <NavLink to="/" element>
-        <SlArrowLeft className="icon-nav" />
+        <FaChevronLeft className="icon-nav" />
       </NavLink>
-      <h2 className="heading">Countries Statistics</h2>
+      <h1 className="heading">Countries</h1>
 
       <div className="position">
         <FaMicrophone className="icon-nav" />
